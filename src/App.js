52a2c56f32dfde3b5e6e811f9785b70306c6d285
Austin Chabaud <austinchabaud@Austins-MacBook-Pro.local>
2021-06-08 18:28:44 -0500

    newcom

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2> AChabaud New App</h2>
+        <h2> addition to new</h2>
       </header>
     </div>
   );
